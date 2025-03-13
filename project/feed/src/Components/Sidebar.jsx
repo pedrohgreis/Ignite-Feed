445cb0ea959ff196734ef09c57751430260dd0c8
Pedro <pedrohgdreis@gmail.com>
2025-03-13 13:24:44 -0400

    feat: sidebar finalizada

--- project/feed/src/Components/Sidebar.jsx
+++ project/feed/src/Components/Sidebar.jsx
@@ -1,17 +1,21 @@
 import styles from './Sidebar.module.css';
 
+import { PencilLine } from 'phosphor-react';
+
 const Sidebar = () => {
   return (
     <div>
       <aside className={styles.sidebar}>
         <img className={styles.cover} src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
         <div className={styles.profile}>
-          <strong>Caio Fernandes</strong>
+          <img className={styles.avatar} src="https://github.com/pedrohgreis.png" alt="My Picture" />
+          <strong>Pedro Reis</strong>
           <span>Web Developer</span>
         </div>
 
         <footer>
           <a href="#">
+            <PencilLine size={20} />
             Editar seu perfil
           </a>
         </footer>
