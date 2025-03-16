1fecf2e826a406d91c927f50b8a456c99d76a3ab
Pedro <pedrohgdreis@gmail.com>
2025-03-16 01:04:07 -0400

    feat: Atualizando Post

--- project/feed/src/App.jsx
+++ project/feed/src/App.jsx
@@ -7,9 +7,58 @@ import Header from './Components/Header';
 import Sidebar from './Components/Sidebar';
 import Post from './Components/Post';
 
+ // Post para ser exibido precisa de indformações como autor e conteúdo
+  // author: {avatar_url: "", name: "", role: ""}
+  // publishdAt: Date  
+  // content: string
+
+const post = [
+    {
+      id: 1,
+      author: {
+        avatar_url: "https://github.com/pedrohgreis.png",
+        name: "Pedro Henrique",
+        role: "Web Developer",
+      },
+
+      content: [
+        {type: "paragraph", content: 'Olá Pessoal'},
+        {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz para a Disney'},
+        {type: 'link', content: 'pedro.developer/webCode'},
+        
+        {type: 'link', content:'#novoprojeto  '},
+        {type: 'link', content:'#disney'} 
+      ],
+
+      publishedAt: new Date('2025-03-15 13:00:00'),
+  },
+
+
+
+  {
+    id: 2,
+    author: {
+      avatar_url: "https://github.com/IuryAlmeidaDev.png",
+      name: "Iury Almeida",
+      role: "Java Developer",
+    },
+
+    content: [
+      {type: "paragraph", content: 'Olá Pessoal'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz para a Disney'},
+      {type: 'link', content: 'iury.developer/webCode'},
+      
+      {type: 'link', content:'#novoprojeto '},
+      {type: 'link', content:'#disney'} 
+    ],
+
+    publishedAt: new Date('2025-03-18 14:32:00'),
+},
+]
+
 
 function App() {
-  
+
 
   return (
     <>
@@ -18,15 +67,15 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Pedro Henrique"
-            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc ultricies ultricies. Nullam nec purus nec nunc ultricies ultricies."
-          />
-
-          <Post
-            author="Pedro Henrique"
-            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc ultricies ultricies. Nullam nec purus nec nunc ultricies ultricies."
-          />
+          {/* iterando post */}
+          {post.map((post) => (
+            <Post 
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt} 
+            />
+          ))}
         </main>
       </div>
       
--- project/feed/src/Components/Post.jsx
+++ project/feed/src/Components/Post.jsx
@@ -4,30 +4,43 @@ import styles from './Post.module.css';
 import Comment from './Comment';
 import Avatar from './Avatar';
 
-const Post = () => {
+// Data
+import {format, formatDistanceToNow} from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
+
+// Para não utilizar o props.author.avatar_url, props.author.name, props.author.role, podemos utilizar o destructuring
+
+const Post = ({author, publishedAt, content}) => {
+  // utilizando o data fns para formatar a data
+  const date = format(publishedAt, "dd 'de' LLLL 'às' HH:mm'h'", {locale: ptBR});
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {locale: ptBR, addSuffix: true});
+
   return (
     
       <article className={styles.post}>
         <header className={styles.postHeader}>
           <div className={styles.author}>
-            <Avatar src="https://github.com/pedrohgreis.png" />
+            <Avatar src= {author.avatar_url} />
             <div className={styles.authorInfo}>
-              <strong>Pedro Reis</strong>
-              <span>Web Developer</span>
+              <strong>{author.name}</strong>
+              <span>{author.role}</span>
             </div>
           </div>
 
-          <time title='13 de Março às 21:32h' dateTime="2025-03-13 21:32:40">Publicado há 1h</time>
+          <time title={date} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
         </header>
 
         <div className={styles.content}>
-          <p>Olá Pessoal</p>
-          <p>Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz para a Disney</p>
-          <p><a href="#">pedro.developer/webCode</a></p>
-          <p>
-            <a href="#">#novoprojeto </a> 
-            <a href="#"> #disney</a>
-          </p>
+          {content.map(line => {
+            if (line.type === 'paragraph') {
+              return <p key={line.content}>{line.content}</p>;
+            } else if (line.type === 'link') {
+              return <p key={line.content}><a href="#">{line.content}</a></p>;
+            }
+            return null; // Se não for nenhum dos dois, retorna null
+          })}
         </div>
 
         <form className={styles.commentForm}>
